c924e5505355369846ac671d22fe2ae798488d06
CamiloParraS <juanparracsfn@gmail.com>
2025-07-07 16:37:03 -0500

    Completed 06_repeatString

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(str, times) {
+    // Validate if the number is positive
+    if (times < 0) {
+        return "ERROR";
+    }
+    let repeated = "";
+    for (let i = 0; i < times; i++) {
+        repeated += str;
+    }
+    return repeated;
 };
+
+console.log(repeatString("HOLA", 4));
 
 // Do not edit below this line
 module.exports = repeatString;
