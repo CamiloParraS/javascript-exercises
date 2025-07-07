63abb0b53a5f51cef528c25755a7bcc25ad9678a
CamiloParraS <juanparracsfn@gmail.com>
2025-07-07 16:58:04 -0500

    completed excercise 07-reverseString

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function (str) {
+  let reverseStr = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    reverseStr += str.charAt(i);
+  }
+  return reverseStr;
 };
+
+console.log(reverseString("Hola Mundo"));
 
 // Do not edit below this line
 module.exports = reverseString;
