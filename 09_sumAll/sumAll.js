f09f43d3c3fc7b0e523daa1c986e4e53659114b2
CamiloParraS <juanparracsfn@gmail.com>
2025-07-07 20:09:04 -0500

    completed excercise 09_sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,25 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+  // CHECK FOR INVALID OUTPUTS
+  if (
+    typeof a !== "number" ||
+    typeof b !== "number" ||
+    a < 0 ||
+    b < 0 ||
+    !Number.isInteger(a) ||
+    !Number.isInteger(b)
+  ) {
+    return "ERROR";
+  }
 
+  let sumTotal = 0;
+  if (a > b) [a, b] = [b, a];
+  for (let i = a; i <= b; i++) {
+    sumTotal += i;
+  }
+  return sumTotal;
 };
+
+// console.log(sumAll(2, 4))
 
 // Do not edit below this line
 module.exports = sumAll;
