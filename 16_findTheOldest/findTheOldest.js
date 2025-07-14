09a03698ce34ffe98095b6046104b3f421d7968b
CamiloParraS <juanparracsfn@gmail.com>
2025-07-14 00:08:34 -0500

    Completed Exercise 16: findTheOldest function

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,35 @@
-const findTheOldest = function() {
+const people = [
+  {
+    name: "Carly",
+    yearOfBirth: 1942,
+    yearOfDeath: 1970,
+  },
+  {
+    name: "Ray",
+    yearOfBirth: 1962,
+    yearOfDeath: 2011,
+  },
+  {
+    name: "Jane",
+    yearOfBirth: 1912,
+    yearOfDeath: 1941,
+  },
+];
 
-};
+function findTheOldest(arr) {
+  return arr.reduce((oldest, person) => {
+    today = new Date().getFullYear();
+
+    const currentYearOfDeath = person.yearOfDeath ?? today;
+    const oldestYearOfDeath = oldest.yearOfDeath ?? today;
+
+    const currentYears = currentYearOfDeath - person.yearOfBirth;
+    const oldestYears = oldestYearOfDeath - oldest.yearOfBirth;
+    return currentYears > oldestYears ? person : oldest;
+  });
+}
+
+console.log(findTheOldest(people));
 
 // Do not edit below this line
 module.exports = findTheOldest;
