943bc669cda726a04a7e310dffbdde0ed5088bc8
CamiloParraS <juanparracsfn@gmail.com>
2025-07-13 23:37:15 -0500

    Completed Exercise 14: Fibonacci sequence

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,7 @@
-const fibonacci = function() {
-
+const fibonacci = function (n) {
+  const sqrt5 = Math.sqrt(5);
+  const phi = (1 + sqrt5) / 2;
+  return Math.round(Math.pow(phi, n) / sqrt5);
 };
 
 // Do not edit below this line
