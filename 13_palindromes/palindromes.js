f832412c37ccf9b1aa6299f3bb74e229a0a9b54a
CamiloParraS <juanparracsfn@gmail.com>
2025-07-13 23:18:59 -0500

    Completed Exercise 13: Palindrome checker

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  let strProcessed = str.toLowerCase().replace(/[^a-z0-9]/g, "");
+  let reversedStr = strProcessed.split("").reverse().join("");
+  return strProcessed === reversedStr;
 };
 
 // Do not edit below this line
